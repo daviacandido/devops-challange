bf3cda9c21dee10176a6c59c414ea4d765f01243
Davi A Candido <daviaraujocandido@gmail.com>
2025-12-19 20:52:04 -0300

    fix: format do dns-records.tf

--- infra/terraform/dns-records.tf
+++ infra/terraform/dns-records.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 resource "cloudflare_dns_record" "bry" {
-  count   = local.create_bry_record ? 1 : 0
+  count = local.create_bry_record ? 1 : 0
 
   zone_id = var.cloudflare_zone_id
   name    = "bry"
